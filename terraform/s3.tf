f3559141711fb979a6de5bb14fe8d96fe2beaa07
1cnlemttmkxr <hohongthuy001@gmail.com>
2024-03-22 10:43:04 +0700

    deploy terraform to S3 aws

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "example" {
-  bucket = "thuyho-s3bucket-aws" #update your account here
+  bucket = "thuy-s3bucket-aws" #update your account here
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
